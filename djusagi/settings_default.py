ac902bc2dc68c93713c9b107c996da98b8ac1d83
Bender <plungerman@gmail.com>
2015-10-31 17:14:56 -0500

    added cache infrastructure to settings

--- djusagi/settings_default.py
+++ djusagi/settings_default.py
@@ -111,6 +111,22 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.debug",
     "django.core.context_processors.media",
 )
+# caching
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+        #'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        #'LOCATION': '127.0.0.1:11211',
+        #'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        #'LOCATION': '/var/tmp/django_directory_cache',
+        #'TIMEOUT': None, # never expires
+        #'KEY_PREFIX': "DJUSAGI_",
+        #'OPTIONS': {
+            #'MAX_ENTRIES': 900000,
+            #'CULL_FREQUENCY': 4,
+        #}
+    }
+}
 # LDAP Constants
 LDAP_SERVER = ''
 LDAP_PORT = ''
