0dd3acd8702f6c24695be3a69293342710e45df2
Bender <plungerman@gmail.com>
2015-11-03 15:41:08 -0600

    removed unused view and imports; replaced login_required with group_required for home view

--- djusagi/core/views.py
+++ djusagi/core/views.py
@@ -1,19 +1,11 @@
 from django.conf import settings
 from django.template import RequestContext
-from django.http import HttpResponseRedirect
-from django.http import HttpResponseBadRequest
 from django.shortcuts import render_to_response
-from django.contrib.auth.decorators import login_required
 
-from djusagi.core.models import CredentialsModel
-from djusagi.core.utils import get_flow
+from djtools.decorators.auth import group_required
 
-from oauth2client import xsrfutil
-from oauth2client.django_orm import Storage
 
-import os
-
-@login_required
+@group_required(settings.ADMINISTRATORS_GROUP)
 def home(request):
     """
     Home page view
@@ -22,25 +14,3 @@ def home(request):
         "home.html",
         context_instance=RequestContext(request)
     )
-
-@login_required
-def oauth2_callback(request):
-    #flow = get_flow(request.session["scope"])
-    flow = get_flow("plus.me")
-    val = xsrfutil.validate_token(
-        settings.SECRET_KEY, request.REQUEST['state'], request.user
-    )
-    if not val:
-        return  HttpResponseBadRequest()
-        '''
-        return render_to_response(
-            'core/debug.html', {
-                'req':request.REQUEST,'val':val,'key':settings.SECRET_KEY
-            },
-             context_instance=RequestContext(request)
-        )
-        '''
-    credential = flow.step2_exchange(request.REQUEST)
-    storage = Storage(CredentialsModel, 'user', request.user, 'credential')
-    storage.put(credential)
-    return HttpResponseRedirect(request.META["HTTP_REFERER"])
