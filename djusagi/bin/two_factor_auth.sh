b81861d6f00f02a8982d49c15bb4b2147a027232
plungerman <plungerman@gmail.com>
2017-07-01 15:45:04 -0500

    added environment variables to script so that informix queries work

--- djusagi/bin/two_factor_auth.sh
+++ djusagi/bin/two_factor_auth.sh
@@ -1,7 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# Informix crap
+INFORMIXSERVER=wilson
+DBSERVERNAME=wilson
+ONCONFIG=onconf.cars
+INFORMIXDIR=/opt/ibm/informix
+INFORMIXSQLHOSTS=$INFORMIXDIR/etc/sqlhosts
+LD_LIBRARY_PATH=$INFORMIXDIR/lib:$INFORMIXDIR/lib/esql:$INFORMIXDIR/lib/tools:$INFORMIXDIR/lib/cli:/usr/local/lib/;
+LD_RUN_PATH=$LD_LIBRARY_PATH
+# unixodbc
+ODBCINI=/etc/odbc.ini
 
 for group in staff faculty students
 do
-    echo "Group: $group"
-    /usr/bin/python /d2/django_projects/djusagi/bin/two_factor_auth.py --who=$group --cache --test
+    /usr/bin/python /d2/django_projects/djusagi/bin/two_factor_auth.py --who=$group --test
 done
